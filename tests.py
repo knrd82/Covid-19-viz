72a67b609bbed83bce80ed5700b746816cf8bcce
knrd82 <kdobrowolski1@gmail.com>
2020-03-19 19:07:12 +0000

    Initial commit

---
+++ tests.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+# fig = go.Figure(data=go.Bar(y=[2, 3, 1]))
+# fig.show()
+# fig.write_html('first_figure.html', auto_open=True)
+
+df = pd.read_csv('Confirmed.csv')
+
